572c7c7b5b2c520c8dab299238433581f348bba1
Tim Catana <32402543+TimCatana@users.noreply.github.com>
2020-03-17 18:48:53 -0400

    Delete openstudy.bat

--- openstudy.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-start chrome.exe https://drive.google.com/drive/u/1/folders/18Fmd3_MvvrvT5V74rqNiyjQ571erGRuY
-start https://biblehub.com/genesis/1-1.htm
-start https://biblehub.com/interlinear/study/john/1.htm#19
-start https://biblehub.com/interlinear/matthew/1.htm
-start https://biblehub.com/interwovengospels/
-exit 
