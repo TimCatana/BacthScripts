f48a4db2f370521b57e9eea0fce19666b9e96be0
TimCatana <timothycatana@gmail.com>
2021-01-07 15:04:51 -0500

    fixed filsesorter

--- fileSorter.bat
+++ fileSorter.bat
@@ -4,47 +4,48 @@ rem only moves common formats
 :LOOP
 set /p director= Enter the directory you wish to sort or enter "exit" (all lower case) to terminate the program:
 
-:: check for exit
+rem check for exit
 if "%director%"=="exit" goto :END 
 
-:: check to see if inputted directory is valid
+rem check to see if inputted directory is valid
 if exist %director% ( echo beginning sorting process ) ELSE ( cls && echo Invalid Directory - please try again && goto :LOOP )
 
-:: create folders inside given directory if they don't exist already
+rem create folders inside given directory if they don't exist already
 if not exist %director%\Photos (mkdir %director%\Photos)
 if not exist %director%\Videos (mkdir %director%\Videos)
 if not exist %director%\Documents (mkdir %director%\Documents)
 if not exist %director%\Music (mkdir %director%\Music)
 
-:: sort documents
-move *.docx %director%\Documents
-move *.pdf %director%\Documents
-move *.xls %director%\Documents
-move *.txt %director%\Documents
+rem sort documents
+move %director%\*.docx %director%\Documents
+move %director%\*.pdf %director%\Documents
+move %director%\*.xls %director%\Documents
+move %director%\*.txt %director%\Documents
 
-:: sort music
-move *.mp3 %director%\Music
-move *.wav %director%\Music
+rem sort music
+move %director%\*.mp3 %director%\Music
+move %director%\*.wav %director%\Music
 
-:: sort pictures
-move *.jpg %director%\Photos
-move *.jpeg %director%\Photos
-move *.tif %director%\Photos
-move *.png %director%\Photos
-move *.raw %director%\Photos
-move *.gif %director%\Photos
-move *.bmp %director%\Photos
+rem sort pictures
+move %director%\*.jpg %director%\Photos
+move %director%\*.jpeg %director%\Photos
+move %director%\*.tif %director%\Photos
+move %director%\*.png %director%\Photos
+move %director%\*.raw %director%\Photos
+move %director%\*.gif %director%\Photos
+move %director%\*.bmp %director%\Photos
 
-:: sort videos
-move *.mp4 %director%\Videos
-move *.wmv %director%\Videos
-move *.avi %director%\Videos
-move *.mkv %director%\Videos
-move *.mov %director%\Videos
+rem sort videos
+move %director%\*.mp4 %director%\Videos
+move %director%\*.wmv %director%\Videos
+move %director%\*.avi %director%\Videos
+move %director%\*.mkv %director%\Videos
+move %director%\*.mov %director%\Videos
+move %director%\*.mts %director%\Videos
 
 echo Done!
 goto :LOOP
 
-:: end of script
+rem end of script
 :END
 exit
